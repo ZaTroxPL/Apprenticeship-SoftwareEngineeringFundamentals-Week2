1603c1a4987be724c8165de9e4fe01b7b370aab0
Jakub Panczyszyn <zatroxkp@gmail.com>
2021-04-29 12:34:46 +0100

    fixed typo

--- LeapYearCal.py
+++ LeapYearCal.py
@@ -2,13 +2,13 @@
 while True:
     year = int(input("Please enter a year: "))
 
-    remeinder_by_4 = year % 4
-    remeinder_by_100 = year % 100
-    remeinder_by_400 = year % 400
+    remainder_by_4 = year % 4
+    remainder_by_100 = year % 100
+    remainder_by_400 = year % 400
 
-    if remeinder_by_4 == 0 and remeinder_by_100 != 0:
+    if remainder_by_4 == 0 and remainder_by_100 != 0:
         print(f"{year} is a leap year")
-    elif remeinder_by_400 == 0:
+    elif remainder_by_400 == 0:
         print(f"{year} is a leap year")
     else:
         print(f"{year} is NOT a leap year")
